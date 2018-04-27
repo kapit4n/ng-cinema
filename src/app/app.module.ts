1bc45109504e2b9a366c3d52fd895a7c4def9881
Luis Arce <luis.arce22@gmail.com>
2018-04-27 16:30:08 -0400

    Adding routers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,33 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { CarouselModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
+import { TodaysComponent } from './components/todays/todays.component';
+import { IncomingComponent } from './components/incoming/incoming.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+const appRoutes: Routes = [
+  { path: 'todays', component: TodaysComponent },
+  { path: 'incoming', component: IncomingComponent },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodaysComponent,
+    IncomingComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     TooltipModule.forRoot(),
     CarouselModule.forRoot()
